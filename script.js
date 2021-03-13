728f7a6f9bddf38b6e935bfca2348d0902750945
Alex Jones <beautydesign.technique@gmail.com>
2021-03-13 14:58:11 -0500

    Corrected icon slideshow, added styling and 3D effects to cards in section 2

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-const icons = document.querySelectorAll('.section-1-icons i')
-let i = 1
+const icons = document.querySelectorAll('.section-1-icons i');
+let i = 1;
 
 setInterval(() => {
-  i++
+  i++;
+  const icon = document.querySelector('.section-1-icons .change');
+  icon.classList.remove('change');
 
-  const icon = document.querySelector('.section-1-icons .change')
-
-  icon.classList.remove('change')
-
-  if(i > icons.length) {
-    icons[0].classList.add('change')
-    i = 1
+  if (i > icons.length) {
+    icons[0].classList.add('change');
+    i = 1;
   } else {
-    icon.nextElementSibling.classList.add('change')
+    icon.nextElementSibling.classList.add('change');
   }
-}, 3800)
+}, 3800);
